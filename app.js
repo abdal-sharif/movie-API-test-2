f2ee1d2bec38c9131557ca7189d912aa980bc6ee
unknown <apdulahisherrif@gmail.com>
2022-07-02 16:47:59 +0300

    update

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const booktitle = document.getElementById('book-title').value;
-const bookauthor = document.getElementById('book-author').value;
-const bookname = document.getElementById('book-name').value;
-const book_author = document.getElementById('book-author').value;
-const bookcover = document.getElementById('cover').value;
+let countryName = document.getElementById('country-h3');
+let countryFlag = document.getElementById('cover');
+let searchBtn = document.getElementById('search');
 
+searchBtn.addEventListener("submit",(e)=>{
+   e.preventDefault();
+   fetch(`https://restcountries.com/v3.1/name/${e.target[0].value}`)
+   .then((res) => res.json())
+   .then((data) =>{
+    console.log(data);
+    countryName.textContent = data[0].name.common;
+    countryFlag.innerHTML = `<img src = ${data[0].flags.png} alt= "country flag">`;
+
+   });
+})
