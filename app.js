82636a771403e07829b07e3543b840a695a0e621
unknown <apdulahisherrif@gmail.com>
2022-07-02 14:34:00 +0300

    inital commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const booktitle = document.getElementById('book-title').value;
+const bookauthor = document.getElementById('book-author').value;
+const bookname = document.getElementById('book-name').value;
+const book_author = document.getElementById('book-author').value;
+const bookcover = document.getElementById('cover').value;
+
